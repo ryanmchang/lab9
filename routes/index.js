cfe873b87085b9b32721e09a5dce992d7c1e6f29
ryanmchang <ryanmchang@gmail.com>
2020-03-04 23:42:30 -0800

    Firt commit

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,15 @@
 var labs = require('../labs.json');
 
 exports.view = function(req, res){
-  if(labs.length < 9) {
+  /*if(labs.length < 9) {
   	var todaysLab = [{
 		"id": "lab-9",
 		"title": "Debugging 101",
 		"date": "March 7",
 		"image": "debug.png"
 	}];
-
   	labs.push(todaysLab);
-  }
+  }*/
 
   res.render('index', {
 	'labs': labs
